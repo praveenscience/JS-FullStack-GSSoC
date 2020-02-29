d373b294775865bf8337225f8f62f0a12d6196a3
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2020-02-29 11:48:22 +0000

    Initialised server with a GET request.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json("Hello, GSSoC!");
+});
+
 app.listen(3100, () => {
   console.log("API Server Started in port 3100!");
 });
