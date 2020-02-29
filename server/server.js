3c1f62b98e5d77a917a4c7bc4fb2cd0250115edd
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2020-02-29 12:06:57 +0000

    Added two end-points for API.

--- server/server.js
+++ server/server.js
@@ -5,6 +5,15 @@ app.get("/", (req, res) => {
   res.json("Hello, GSSoC!");
 });
 
+app.get("/api/users", (req, res) => {
+  const Users = ["Alpha", "Beta", "Gamma", "Delta"];
+  res.json(Users);
+});
+
+app.get("/api/sayHello", (req, res) => {
+  res.json("Hello, GSSoC!");
+});
+
 app.listen(3100, () => {
   console.log("API Server Started in port 3100!");
 });
