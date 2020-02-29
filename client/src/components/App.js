716695c686c54bc71b81b6f85cac6a2b9d26c7ff
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2020-02-29 12:25:00 +0000

    Displayed the users from the API.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
+import { GetUsers } from "../services/APIServer";
 
 export default class App extends Component {
   state = {
     Users: []
   };
+  componentDidMount() {
+    GetUsers().then(res => {
+      this.setState({
+        Users: res.data
+      });
+    });
+  }
   render() {
     const { Users } = this.state;
     return (
       <>
         <h1>Users!</h1>
         <div>Hello, GSSoC</div>
-        {Users.length > 0 && <ul></ul>}
+        {Users.length > 0 && (
+          <ul>
+            {Users.map((user, key) => (
+              <li key={key}>{user}</li>
+            ))}
+          </ul>
+        )}
       </>
     );
   }
